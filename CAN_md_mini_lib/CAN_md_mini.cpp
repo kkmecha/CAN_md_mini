92515841811d55179a3c7a1c2a8f19413c452d23
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-06-15 11:26:54 +0900

    Update CAN_md_mini.cpp

--- CAN_md_mini_lib/CAN_md_mini.cpp
+++ CAN_md_mini_lib/CAN_md_mini.cpp
@@ -1,22 +1,22 @@
+// 制御値の符号を取って送信する必要がある
 #include "CAN_md_mini.h"
 #include <cstdint>
 #include <cmath>
 
 CAN_md_mini::CAN_md_mini(CAN &can, char motor_num) 
         : _can(can), _motor_num(motor_num) {
-    _can.frequency(1000000);
-    _can.mode(CAN::Normal);
-    _motor_max = 10000;
-    // その他処理など
+            _can.frequency(1000000);
+            _can.mode(CAN::Normal);
+            _motor_max = 10000;
 }
 
 int CAN_md_mini::send(int speed){ // とりあえずモーター1個分だけ
     _msg.id = 0x300 + *((uint32_t *)_id_ptr);
     _msg.len = 8;
 
-    if(abs(speed) >= 10000){
-        _msg.data[0] = Brake;
-        _msg.data[1] = CW;
+    int _abs_speed = abs(speed);
+    if(_abs_speed >= 10000){
+        _abs_speed = 9999;
     }
     if(0 < speed){
         _msg.data[0] = Rotate;
@@ -27,13 +27,13 @@ int CAN_md_mini::send(int speed){ // とりあえずモーター1個分だけ
     }else{
         _msg.data[0] = Brake;
         _msg.data[1] = CW;
+        _abs_speed = 0;
     }
 
-    _msg.data[2] = (speed >> 8);
-    _msg.data[3] = (speed & 0xff);
+    _msg.data[2] = (_abs_speed >> 8);
+    _msg.data[3] = (_abs_speed & 0xff);
 
-    if(_over) _i = 2; else _i = 4;
-    for( ; _i < 8; _i++)
+    for(_i = 4 ; _i < 8; _i++)
         _msg.data[_i] = 0; // Nullを残さない
     
     if(_can.write(_msg)){
@@ -42,24 +42,4 @@ int CAN_md_mini::send(int speed){ // とりあえずモーター1個分だけ
     else{
         return -1;
     } 
-
-    // if(0 < *speed_arr){ // 制御信号以外のビットのセット
-    //     _data |= Rotate;
-    //     _data |= CW;
-    // }else if(*speed_arr < 0){
-    //     _data |= Rotate;
-    //     _data |= CCW;
-    // }else{
-    //     _data |= Brake;
-    //     _data |= CW; // セットしているが関係ない
-    // }
-
-    // char _byte_top[_motor_num * 2], _byte_sub[_motor_num * 2];
-    // int _a = 0;
-
-    // for(int i=0;i<_motor_num;i++){
-    //     if(speed_arr[i]>=_motor_max)return 0;
-    //     _byte_top[_a++] = (char)(speed_arr[i] >> 8);
-    //     _byte_sub[_a++] = (char)(speed_arr[i] & 0xFF);
-    // }
 }
