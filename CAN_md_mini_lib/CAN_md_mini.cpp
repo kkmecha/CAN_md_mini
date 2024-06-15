a0b4a55e432016c2e2f295d7137fd2d063d957a6
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-06-15 11:55:31 +0900

    Update CAN_md_mini.cpp

--- CAN_md_mini_lib/CAN_md_mini.cpp
+++ CAN_md_mini_lib/CAN_md_mini.cpp
@@ -9,36 +9,37 @@ CAN_md_mini::CAN_md_mini(CAN &can, char motor_num)
             _motor_max = 10000;
 }
 
-int CAN_md_mini::send(int speed){ // とりあえずモーター1個分だけ
-    _msg.id = 0x300 + *((uint32_t *)_id_ptr);
-    _msg.len = 8;
+int CAN_md_mini::send(int *speed){ // とりあえずモーター1個分だけ
 
-    int _abs_speed = abs(speed);
-    if(_abs_speed >= 10000){
-        _abs_speed = 9999;
-    }
-    if(0 < speed){
-        _msg.data[0] = Rotate;
-        _msg.data[1] = CW;
-    }else if(speed < 0){
-        _msg.data[0] = Rotate;
-        _msg.data[1] = CCW;
-    }else{
-        _msg.data[0] = Brake;
-        _msg.data[1] = CW;
-        _abs_speed = 0;
-    }
+    for(int _id = 0; _id < _motor_num; _id++){
+        _msg.id = 0x300 + _id;
+        _msg.len = 8;
 
-    _msg.data[2] = (_abs_speed >> 8);
-    _msg.data[3] = (_abs_speed & 0xff);
+        int _abs_speed = abs(speed[_id]);
+        if(_abs_speed >= 10000){
+            _abs_speed = 9999;
+        }
+        if(0 < speed[_id]){
+            _msg.data[0] = Rotate;
+            _msg.data[1] = CW;
+        }else if(speed[_id] < 0){
+            _msg.data[0] = Rotate;
+            _msg.data[1] = CCW;
+        }else{
+            _msg.data[0] = Brake;
+            _msg.data[1] = CW;
+            _abs_speed = 0;
+        }
 
-    for(_i = 4 ; _i < 8; _i++)
-        _msg.data[_i] = 0; // Nullを残さない
-    
-    if(_can.write(_msg)){
-        return 1;
+        _msg.data[2] = (_abs_speed >> 8);
+        _msg.data[3] = (_abs_speed & 0xff);
+
+        for(_i = 4 ; _i < 8; _i++)
+            _msg.data[_i] = 0; // Nullを残さない
+        
+        if(!_can.write(_msg)){
+            return -1;
+        } 
     }
-    else{
-        return -1;
-    } 
+    return 1;
 }
