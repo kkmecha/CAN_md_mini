d12d740d139cf95cace825396fbb09549f71ab25
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-06-15 12:34:57 +0900

    Update CAN_md_mini.h

--- CAN_md_mini_lib/CAN_md_mini.h
+++ CAN_md_mini_lib/CAN_md_mini.h
@@ -7,24 +7,23 @@
 
 class CAN_md_mini{
     public:
-        CAN_md_mini(CAN &can, int motor_num);
-        int send(int *speed);
-    private:
-        enum Brake_mask{
-            Rotate,
-            Brake,
-        };
-        enum Direction_mask{
-            CW,
-            CCW,
-        };
-        CAN &_can;
-        CANMessage _msg;
-        bool _over;
-        uint32_t _id_ptr = 0x0800F800;
-        int _i, _motor_num;
-        
-};
+    CAN_md_mini(CAN &can, int motor_num);
+    int send(int *speed);
 
+    private:
+    enum Brake_mask{
+        Rotate,
+        Brake,
+    };
+    enum Direction_mask{
+        CW,
+        CCW,
+    };
+    CAN &_can;
+    CANMessage _msg;
+    bool _over;
+    uint32_t _id_ptr = 0x0800F800;
+    int _i, _motor_num;   
+};
 
 #endif // __INCLUDE_CAN_MD_MINI_H__
